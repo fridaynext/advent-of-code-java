72e2872525fea5cd72df76df2952fd182d1b5c81
Casey Friday <casey@friday-next.com>
2020-12-10 00:08:58 -0600

    adding smallest and largest num

--- src/main/java/com/fridaynext/adventofcode/model/Numbers.java
+++ src/main/java/com/fridaynext/adventofcode/model/Numbers.java
@@ -13,6 +13,7 @@ public class Numbers {
         // Need a register of 25 to hold previous nums
         ArrayList<Double> prevTwentyFive = new ArrayList<>();
         int index = 0;
+        double invalidNum = 0;
         for (String xmasNumber : xmasNumbers.getFileLines() ) {
             // Convert it to a number
             double xmasInt = Double.parseDouble(xmasNumber);
@@ -36,10 +37,23 @@ public class Numbers {
                 // now we've checked all the numbers, let's see if we found a match
                 if (!foundMatch) {
                     double smallestNum = 0;
+                    double biggestNum = 0;
                     for (double thisNum : prevTwentyFive) {
-
+                        if (thisNum > biggestNum) {
+                            biggestNum = thisNum;
+                        }
                     }
+                    smallestNum = biggestNum;
+                    for (double thisNum : prevTwentyFive) {
+                        if (thisNum < smallestNum) {
+                            smallestNum = thisNum;
+                        }
+                    }
+                    // System.out.println("Sum of magic = " + (smallestNum + biggestNum));
+                    // System.out.printf("Sum of Magic: %.0f\n", (smallestNum + biggestNum));
                     System.out.println(xmasInt + " did not pass the test");
+                    invalidNum = xmasInt;
+                    break;
                 }
 
                 // we have 25, so let's start popping these puppies out
@@ -48,6 +62,38 @@ public class Numbers {
             System.out.println("New array length is: " + prevTwentyFive.size());
 
             index++;
+        }
+        ArrayList<Double> storedNums = new ArrayList<>();
+        for (String xmasNumber : xmasNumbers.getFileLines() ) {
+            // invalidNum is what we're looking for
+            storedNums.add(Double.parseDouble(xmasNumber));
+            if (storedNums.size() > 2) {
+                double storageSum = 0;
+                ArrayList<Double> currentLookie = new ArrayList<>();
+                for (int i = storedNums.size()-1; i >= 0; i--) {
+                    currentLookie.add(storedNums.get(i));
+                    storageSum += storedNums.get(i);
+                    if (storageSum == invalidNum) {
+                        // we found it!
+                        double smallestNum = 0;
+                        double biggestNum = 0;
+                        for (double thisNum : currentLookie) {
+                            if (thisNum > biggestNum) {
+                                biggestNum = thisNum;
+                            }
+                        }
+                        smallestNum = biggestNum;
+                        for (double thisNum : currentLookie) {
+                            if (thisNum < smallestNum) {
+                                smallestNum = thisNum;
+                            }
+                        }
+                        System.out.printf("The encryption weakness is: %.0f\n", (smallestNum+biggestNum));
+                    }
+                }
+                currentLookie.clear();
+            }
+
         }
     }
 }
